4008225bf49e18d74f9f6621c7924a22b166e191
im-yu <597307663@qq.com>
2020-03-07 14:49:18 +0800

    3

--- Array/Climbing stairs.py
+++ Array/Climbing stairs.py
@@ -8,4 +8,21 @@ class Solution:
             f1 = f2
             f2 = f3
             f3 = f1 + f2
-        return f3 
+        return f3
+
+
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        memo = (n + 1) * [0]
+
+        def climb(i, n, memo):
+            if i > n:
+                return 0
+            if i == n:
+                return 1
+            if memo[i] > 0:
+                return memo[i]
+            memo[i] = climb(i + 1, n, memo) + climb(i + 2, n, memo)
+            return memo[i]
+
+        return climb(0, n, memo)
---
+++ Tree/Maximum Subsequence Sum.py
@@ -0,0 +1,68 @@
+# K = 10
+# nums = [-10,1,2,3,4,-5,-23,3,7,-21]
+#
+#
+# n = K
+# ans_sum = -1
+# ans_right = n-1
+# ans_left = 0
+# tmp_sum = 0
+# tmp_left = 0
+# for i in range(1, n):
+#     tmp_sum += nums[i]
+#     if tmp_sum < 0:
+#         tmp_sum = 0
+#         tmp_left = i+1
+#     elif tmp_sum > ans_sum:
+#         ans_sum = tmp_sum
+#         ans_right = i
+#         ans_left = tmp_left
+# print("%d %d %d"%(ans_sum,ans_left,ans_right))
+
+
+
+
+#         curr_sum = max(nums[i], curr_sum + nums[i])
+#         max_sum = max(max_sum, curr_sum)
+#
+#     return max_sum
+# print(maxSubArray(nums))
+
+# K = int(input())
+# line = input()
+# nums = list(map(int, line.split()))
+#
+# def maxSubArray(nums):
+#     n = K
+#     curr_sum = max_sum = nums[0]
+#     for i in range(1, n):
+#         curr_sum = max(nums[i], curr_sum + nums[i])
+#         max_sum = max(max_sum, curr_sum)
+#     return max_sum
+# print(maxSubArray(nums))
+
+K = int(input())
+line = input()
+nums = list(map(int, line.split()))
+
+ans_sum = -1
+ans_right = K-1
+ans_left = 0
+tmp_sum = 0
+tmp_left = 0
+for i in range(K):
+    tmp_sum += nums[i]
+    if tmp_sum < 0:
+        tmp_sum = 0
+        tmp_left = i+1
+    elif tmp_sum > ans_sum:
+        ans_sum = tmp_sum
+        ans_right = i
+        ans_left = tmp_left
+
+if ans_sum < 0:ans_sum = 0
+print("%d %d %d" % (ans_sum, nums[ans_left], nums[ans_right]))
+
+
+
+
